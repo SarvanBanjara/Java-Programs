04e35a0a1dde2f9336e6198bb4ed805e529e7213
Sarvan Banjara <164245816+SarvanBanjara@users.noreply.github.com>
2025-08-14 13:55:11 +0530

    Java Program Test

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, DSA World!");
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello from Sarvan Banjara");
+
     }
 }
